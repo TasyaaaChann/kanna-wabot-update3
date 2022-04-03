deef1cce10877ab10aa8ab2ce5c6da78695d490d
TasyaaaChann <102640065+TasyaaaChann@users.noreply.github.com>
2022-04-04 04:25:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,9 +31,9 @@ function pickRandom(list) {
 
 // Global Staff
 global.DeveloperMode = 'false' //Buat Mode True False Moderator
-global.linkGC = ['https://chat.whatsapp.com/Hjr3H7l9iC8F2deP0cv6GT'] // ganti jadi group lu
-global.owner = ['62831433937633', '6283143393763' ,'6288217277973','62831433937630','6285704954639', '62895336282144', '6285712420674'] // Put your number here
-global.mods = ['6285231651905'] // Want some help?
+global.linkGC = ['kosonggg'] // ganti jadi group lu
+global.owner = ['6283830815715', '6283830815715' ,'6283830815715','6283830815715','6283830815715', '6283830815715', '6283830815715'] // Put your number here
+global.mods = ['6283830815715'] // Want some help?
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -69,12 +69,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '🌙 Ƙαnnα вσт\nwa.me/6283143393763\n\n\n𝙱𝚢 𝙻𝚎𝚝𝚝𝚊 あ\n\n\nFollow IG: @ppiowy_\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.'
+global.packname = '🥀 Ｔａｓｙａ\nwa.me/6283830815715\n\n\n𝙱𝚢 Ｔａｓｙａ ⸙\n\n\nFollow IG: @TasyaaUwuu\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.'
 global.author = '۰ ⸼'
 
 // BOT WM
-global.botwm = '▧ Kαnnα BOT | © ℓetta - sαmα' //UBAH SETERAHMU
-global.wm = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Ƙαnnα - chαn˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.botwm = 'Ｔａｓｙａ B O T | © Ｔａｓｙａ' //UBAH SETERAHMU
+global.wm = 'Ｔａｓｙａ??'
 global.botdate = `❏ ${wktuh} H  ${wktum} M  ${wktus} S\n❏ ${week} ${date}`
 
 // LOGO
