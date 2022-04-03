8866e57a48ab99e25c5635575d7615545ce350ac
TasyaaaChann <102640065+TasyaaaChann@users.noreply.github.com>
2022-04-04 04:28:10 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -16,8 +16,8 @@ menemukan bug?
  ${ow} Hanya menjawab chat seputar BOT`
   await conn.sendMessage(m.chat, {
     contacts: [{
-    "displayName": "ᴏᴡɴᴇʀ ᴋᴀɴɴᴀʙᴏᴛ",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ᴏᴡɴᴇʀ ᴋᴀɴɴᴀʙᴏᴛ\nitem1.TEL;waid=62831433937633:62831433937633\nitem1.X-ABLabel:📍 Creator\nitem2.TEL;waid=6283143393763:6283143393763\nitem2.X-ABLabel:📍 Whatsapp Bot\nitem3.EMAIL;type=INTERNET: kannxofc@gmail.com\nitem3.X-ABLabel:💌 Email\nitem4.URL:nekopoi.care\nitem4.X-ABLabel:📮 Rest Api\nitem5.ADR:;;❦ ℒℯ𝓉𝓉𝒶 - 𝒮𝒶𝓃𝒶;;;;\nitem5.X-ABADR:ac\nitem5.X-ABLabel:──────────────\nitem6.X-ABLabel:⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\nEND:VCARD"
+    "displayName": "Ｔａｓｙａ⸙",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Ｔａｓｙａ⸙\nitem1.TEL;waid=6283830815715:Ｔａｓｙａ\nitem1.X-ABLabel:📍 Creator\nitem2.TEL;waid=6283830815715:6283830815715\nitem2.X-ABLabel:📍 Whatsapp Bot\nitem3.EMAIL;type=INTERNET: tasyaa2004@gmail.com\nitem3.X-ABLabel:💌 Email\nitem4.URL:nekopoi.care\nitem4.X-ABLabel:📮 Rest Api\nitem5.ADR:;;Ｔａｓｙａ⸙;;;;\nitem5.X-ABADR:ac\nitem5.X-ABLabel:──────────────\nitem6.X-ABLabel:⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\nEND:VCARD"
   }, {
     "displayName": "Whatsapp",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ\nEND:VCARD"
